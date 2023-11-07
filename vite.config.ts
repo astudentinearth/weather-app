b7ed4c7a85255c9d72f7b5ef1fad3f2b6af70654
astudentinearth <95227070+astudentinearth@users.noreply.github.com>
2023-11-07 22:33:39 +0300

    🎨 Update vite configuration - `@` now resolves to the src folder

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 /// <reference types="vitest" />
 
 // https://vitejs.dev/config/
@@ -9,5 +10,10 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: 'src/setupTests.ts'
+  },
+  resolve:{
+    alias:{
+      '@': path.resolve(__dirname, './src')
+    }
   }
 })
