0dbb0c5888c2495197c3fdba5734f6221ec57df8
Burak <burak.yn.dev@gmail.com>
2024-06-11 16:59:19 +0300

    ➕ setup shadcn-ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
-import './index.css'
+import './globals.css'
 import './i18n.ts'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
