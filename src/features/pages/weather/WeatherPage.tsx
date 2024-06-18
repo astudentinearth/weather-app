cebd95a03413dc203d29927dbf0aa6a71e2ddf91
Burak <burak.yn.dev@gmail.com>
2024-06-18 20:58:34 +0300

    💬 Add attribution text

--- src/features/pages/weather/WeatherPage.tsx
+++ src/features/pages/weather/WeatherPage.tsx
@@ -1,11 +1,14 @@
 //import Navigation from "@/features/navigation";
 import { CurrentWeatherWidget } from "@/features/widgets";
-import { cn } from "@/lib/utils";
 
 export default function WeatherPage(){
-    return <div className="page flex sm:p-2 sm:justify-center sm:items-center overflow-x-hidden">
+    return <div className="page flex flex-col sm:p-2 sm:justify-center sm:items-center overflow-x-hidden">
         <div className="w-full max-w-[900px]">
             <CurrentWeatherWidget></CurrentWeatherWidget>
         </div>
+        <span className="text-sm text-muted-foreground text-center">
+                <a className="hover:text-muted-foreground-hover transition-colors" href="https://open-meteo.com/">Weather data by Open-Meteo.com</a>
+                <a className="hover:text-muted-foreground-hover transition-colors" href="https://creativecommons.org/licenses/by/4.0/"> | CC BY 4.0</a>
+        </span>
     </div>
 }
