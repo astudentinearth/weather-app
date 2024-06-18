3e927311bd271324936e01f40cf1e93b197024ba
Burak <burak.yn.dev@gmail.com>
2024-06-18 21:07:55 +0300

    ⚰️ Get rid of routes

--- src/Layout.tsx
+++ src/Layout.tsx
@@ -1,19 +1,8 @@
 import { createHashRouter, RouterProvider } from "react-router-dom";
-import Navigation from "./features/navigation";
 import WeatherPage from "./features/pages/weather/WeatherPage";
-import { LocationsPage } from "./features/pages/locations";
-import { cn } from "./lib/utils";
 const router = createHashRouter([{
   path: '/',
   element: <WeatherPage></WeatherPage>
-},
-{
-    path: '/settings',
-    element: <Navigation></Navigation>
-},
-{
-    path: '/locations',
-    element: <LocationsPage></LocationsPage>
 }])
 
 
