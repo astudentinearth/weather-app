b714239b3aedbb4594d514cd8eff924cded29eea
Burak <burak.yn.dev@gmail.com>
2024-06-19 22:11:10 +0300

    💄 Improve look on mobile

--- src/features/widgets/CurrentWeather/CurrentWeather.tsx
+++ src/features/widgets/CurrentWeather/CurrentWeather.tsx
@@ -39,7 +39,7 @@ export function CurrentWeatherWidget(){
     },[searchParams, options])
     return <div className="current-weather-widget transition-[font-size,transform] duration-100 text-2xl sm:text-4xl z-20">
         <div className="px-2 flex flex-col gap-3">
-            <div className="hsm:self-start flex w-full gap-1">
+            <div className="hsm:self-start flex w-full gap-1 items-center">
                 <span className="select-none">{`${state?.location.name}`}</span> &nbsp;
                 <span className="text-zinc-500 select-none">{`${new Date().toLocaleDateString()}`}</span>
                 <div className="w-full"></div>
