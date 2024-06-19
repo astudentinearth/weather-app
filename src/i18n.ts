ada7bcdccb57cd7a03044c1e1b5381fc549be0b7
Burak <burak.yn.dev@gmail.com>
2024-06-19 22:53:36 +0300

    🚑 Fix i18next failing to find translations

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,9 +1,14 @@
 import i18n from "i18next"
 import { initReactI18next } from "react-i18next"
 import LanguageDetector from "i18next-browser-languagedetector"
-import Backend from "i18next-http-backend"
+import Backend, { HttpBackendOptions } from "i18next-http-backend"
 
 // Used backend: http
 // Plugins: LanguageDetector
-i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init({fallbackLng: "en", debug: true});
+i18n.use(Backend).use(LanguageDetector).use(initReactI18next).init<HttpBackendOptions>({
+    fallbackLng: "en", debug: true,
+    backend:{
+        loadPath: "/weather-app/locales/{{lng}}/{{ns}}.json"
+    }
+});
 export default i18n;
