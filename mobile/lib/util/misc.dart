e8af5e6b0838b2b19a720369cab9398894b27bf5
Burak <burak.yn.dev@gmail.com>
2024-10-17 21:12:06 +0300

    :technologist: Add nullable cast function

--- mobile/lib/util/misc.dart
+++ mobile/lib/util/misc.dart
@@ -1,2 +1,5 @@
-// Given a dynamic that is known to be either int or double, makes sure it's a double
+/// Given a dynamic that is known to be either int or double, makes sure it's a double
 double ensureDouble(dynamic x) => (x is int ? x.toDouble() : x as double);
+
+/// Performs a type cast for type T, without casting null values
+T? nullableCast<T>(dynamic object) => (object == null) ? object : object as T;
---
+++ mobile/test/util_misc_test.dart
@@ -0,0 +1,16 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:mobile/util/misc.dart';
+
+void main() {
+  test("should cast types without casting null", () {
+    expect(nullableCast<bool>(null), isNull);
+    expect(nullableCast<bool>(true), isTrue);
+  });
+
+  test("should ensure number values are double", (){
+    final case1 = ensureDouble(1243);
+    final case2 = ensureDouble(5.0);
+    expect(case1 == 1243, isTrue);
+    expect(case2 == 5, isTrue);
+  });
+}
